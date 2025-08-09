20e3d552d4e0f31e5c6cc89b079140d5e66d23fd
Chloe Erhardt <erha0006@algonquin.live.com>
2025-08-09 16:44:15 -0400

    Completes the assignment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App';
+import Books from './Books';
+import BookDetail from './BookDetail';
+import Quotes from './Quotes';
+
+import './index.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Books />} />
+          <Route path="books/:id" element={<BookDetail />} />
+          <Route path="quotes" element={<Quotes />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
-)
+);
+
